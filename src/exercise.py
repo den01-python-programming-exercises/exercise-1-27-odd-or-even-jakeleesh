a9f911b61b9939e029587f91c0fe9d2226b61118
Jake Lee <jake.lee@fdmgroup.com>
2021-08-31 13:28:23 +0800

    Attempts solution

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,10 @@
 def main():
     #write your code below this line
+    num = int(input("Give a number:"))
+    if (num % 2 == 0):
+        print("Number " + str(num) + " is even.")
+    else:
+        print("Number " + str(num) + " is odd.")
 
 if __name__ == '__main__':
     main()
